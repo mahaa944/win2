3c2671b93ab560fc040e20c2686ed04649d09c1c
mahaa944 <mmsa944@gmail.com>
2023-11-05 00:01:14 +0300

    init

---
+++ inc/select.php
@@ -0,0 +1,4 @@
+<?php
+$sql= 'SELECT * FROM users' ;
+$result=mysqli_query($conn,$sql);
+$users =mysqli_fetch_all($result,MYSQLI_ASSOC);
---
+++ parts/footer.php
@@ -0,0 +1,4 @@
+<script src="./js/bootstrap.bandle.min.js" ></script>
+ <script src="./js/script.js"></script>
+</body>
+</html>
